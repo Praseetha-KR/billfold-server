f8bbaee20204ae475725dccc1a979885012208bf
Praseetha-KR <voidimagineer@yahoo.com>
2014-12-10 00:42:19 +0530

    CORS enable

--- app/routes/expenses.js
+++ app/routes/expenses.js
@@ -1,8 +1,79 @@
-exports.findAll = function(req, res) {
-	res.send([{name: 'expense1'}, {name:'expense2'}, {name: 'expense3'}]);
-};
+var express = require('express');
+var router = express.Router();
 
-exports.findById = function(req, res) {
-	res.send({id: req.params.id, name: "The Expense", category: "sample_category", amount: "100"});
-};
+var mongoose = require('mongoose');
+var Expense = require('../models/expenses');
 
+
+router.use(function(req, res, next) {
+	console.log('Middleware to use all the requests.');
+	res.header("Access-Control-Allow-Origin", "*");
+ 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+router.get('/', function(req, res) {
+	res.json({ message: 'Welcome to ExpenseApp!'});
+});
+
+router.route('/expenses')
+	.post(function(req, res) {
+		var expense = new Expense();
+
+		expense.category = req.body.category;
+		expense.amount = req.body.amount;
+		expense.date = req.body.date;
+		expense.remark = req.body.remark;
+
+		expense.save(function(err, expense) {
+			if (err) {
+				return console.error(err);
+			}
+			console.log(expense);
+			res.json({ message: 'Expense added!'});
+		});
+	})
+	.get(function(req, res) {
+		Expense.find(function(err, expenses) {
+			if (err)
+				res.send(err);
+			res.json(expenses);
+		});
+	});
+
+router.route('/expenses/:expense_id')
+	.get(function(req, res) {
+		Expense.findById(req.params.expense_id, function(err, expense) {
+			if (err)
+				res.send(err);
+			res.json(expense);
+		});
+	})
+	.put(function(req, res) {
+		Expense.findById(req.params.expense_id, function(err, expense) {
+			if (err)
+				res.send(err);
+
+			expense.category = req.body.category;
+			expense.amount = req.body.amount;
+			expense.date = req.body.date;
+			expense.remark = req.body.remark;
+
+			expense.save(function(err) {
+				if (err)
+					res.send(err);
+				res.json({ message: 'Expense updated!' });
+			});
+		});
+	})
+	.delete(function(req, res) {
+		Expense.remove({
+			_id: req.params.expense_id
+		}, function(err, expense) {
+			if (err)
+				res.send(err);
+			res.json({ message: 'Successfully deleted!' });
+		});
+	});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,80 +10,8 @@ var port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost:27017/expensetestdb');
 
-var Expense = require('./app/models/expenses');
-
-var router = express.Router();
-
-router.use(function(req, res, next) {
-	console.log('Middleware to use all the requests.');
-	next();
-});
-
-router.get('/', function(req, res) {
-	res.json({ message: 'Welcome to ExpenseApp!'});
-});
-
-router.route('/expenses')
-	.post(function(req, res) {
-		var expense = new Expense();
-
-		expense.category = req.body.category;
-		expense.amount = req.body.amount;
-		expense.date = req.body.date;
-		expense.remark = req.body.remark;
-
-		expense.save(function(err, expense) {
-			if (err) {
-				return console.error(err);
-			}
-			console.log(expense);
-			res.json({ message: 'Expense added!'});
-		});
-	})
-	.get(function(req, res) {
-		Expense.find(function(err, expenses) {
-			if (err)
-				res.send(err);
-			res.json(expenses);
-		});
-	});
-
-router.route('/expenses/:expense_id')
-	.get(function(req, res) {
-		Expense.findById(req.params.expense_id, function(err, expense) {
-			if (err)
-				res.send(err);
-			res.json(expense);
-		});
-	})
-	.put(function(req, res) {
-		Expense.findById(req.params.expense_id, function(err, expense) {
-			if (err)
-				res.send(err);
-
-			expense.category = req.body.category;
-			expense.amount = req.body.amount;
-			expense.date = req.body.date;
-			expense.remark = req.body.remark;
-
-			expense.save(function(err) {
-				if (err)
-					res.send(err);
-				res.json({ message: 'Expense updated!' });
-			});
-		});
-	})
-	.delete(function(req, res) {
-		Expense.remove({
-			_id: req.params.expense_id
-		}, function(err, expense) {
-			if (err)
-				res.send(err);
-			res.json({ message: 'Successfully deleted!' });
-		});
-	});
-
-app.use('/api', router);
+var expenseRouter = require('./app/routes/expenses');
+app.use('/api', expenseRouter);
 
 app.listen(port);
 console.log('Express server is listening on port ' + port);
