ebccf7ecf08ccc09d779cfde4a9c6bad12c1e594
Praseetha-KR <praseetha04@gmail.com>
2017-02-11 22:15:59 +0530

    Sample routes for expenses

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,24 @@
 import Router from 'koa-router';
 
-const router = new Router();
-
-router.get('/hello', async (ctx, next) => {
-    ctx.body = "hello";
+const router = new Router({
+    prefix: '/api/v1/'
 });
+
+router
+    .get('/expenses', async (ctx, next) => {
+        ctx.body = "hello";
+    })
+    .post('/expenses', async (ctx, next) => {
+        ctx.body = ctx.request.body;
+    })
+    .get('/expenses/:id', async (ctx, next) => {
+        ctx.body = ctx.params.id;
+    })
+    .put('/expenses/:id', async (ctx, next) => {
+        ctx.body = ctx.params.id;
+    })
+    .del('/expenses/:id', async (ctx, next) => {
+        ctx.body = ctx.params.id;
+    });
 
 export default router;
