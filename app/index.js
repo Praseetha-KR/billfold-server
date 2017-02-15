2f2fc5e61ab52560e87d0c8a42f0dc1ce8da847c
Praseetha-KR <praseetha04@gmail.com>
2017-02-15 23:15:52 +0530

    Include bodyparser

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
 import Koa from 'koa';
 import logger from 'koa-logger';
-import router from './routes'
+import router from './routes';
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 app
     .use(logger())
+    .use(bodyParser())
     .use(router.routes());
 
 export default app;
